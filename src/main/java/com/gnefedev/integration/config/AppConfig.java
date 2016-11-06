69711a8141e0f979cff123a0ab27eeba9e045311
gerakln <georgynefedev@gmail.com>
2016-11-06 12:18:48 +0300

    refactor

--- src/main/java/com/gnefedev/integration/config/AppConfig.java
+++ src/main/java/com/gnefedev/integration/config/AppConfig.java
@@ -2,7 +2,6 @@ package com.gnefedev.integration.config;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 import org.springframework.oxm.Marshaller;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
@@ -10,9 +9,12 @@ import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 /**
  * Created by gerakln on 31.01.16.
  */
-@SpringBootApplication
-@ComponentScan(basePackages = "com.gnefedev.integration.services")
-@Import({JmsConfig.class, IntegrationConf.class, JpaConfig.class})
+@SpringBootApplication(scanBasePackages = "com.gnefedev.integration.services")
+@Import({
+        JmsConfig.class,
+        IntegrationConf.class,
+        JpaConfig.class
+})
 public class AppConfig {
     @Bean(name = "xmlMarshaller")
     public Marshaller jaxb() {
